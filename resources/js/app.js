eaf86b7f40e3fd0778ff1338a9b10e38055f6b52
Alexander Martynov <martyn1201@gmail.com>
2023-07-31 19:11:35 +0400

    migrations + login&register templates

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,7 @@ import './bootstrap';
 import {createApp} from "vue";
 import App from "@/App.vue";
 import router from "@/router.js";
+import '../css/app.css'
 
 
 const app = createApp(App)
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    mode: process.env.NODE_ENV ? 'jit' : undefined,
     content: [
         "./resources/**/*.blade.php",
         "./resources/**/*.js",
